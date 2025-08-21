fc8db54de35f0003b6f8664c2717cc01584ee469
Tavor313 <masson_feed@mail.ru>
2025-08-21 14:24:42 +0500

    Delete api directory

--- api/src/models/AuditLog.js
+++
@@ -1,3 +0,0 @@
-import mongoose from 'mongoose';
-const auditSchema = new mongoose.Schema({ type: String, payload: Object, actorTelegramId: Number }, { timestamps: true });
-export default mongoose.model('AuditLog', auditSchema);
